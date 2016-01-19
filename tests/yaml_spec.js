a89f5a63707bb81480607523076e1a2cc43c53fe
kenjones-cisco <kenjones@cisco.com>
2016-01-18 21:34:19 -0500

    chore(project): Cleanup, debugging, comments
    Adds some debugging, comments, and then simplify.

--- tests/yaml_spec.js
+++ tests/yaml_spec.js
@@ -3,69 +3,68 @@
 var path = require('path');
 
 var confab = require('../lib/index');
-var yaml = require('../lib/yaml');
 
 function yamlFixturePath(name) {
-  return path.join(__dirname, 'fixtures', name + '.yml');
+    return path.join(__dirname, 'fixtures', name + '.yml');
 }
 
 describe('confab-yaml', function () {
 
-  describe('#loadYaml', function () {
+    describe('#loadYaml', function () {
 
-    it('passing in no files gets empty object', function () {
-      var config = confab([
-        confab.loadYaml()
-      ]);
-      expect(config).toEqual({});
+        it('passing in no files gets empty object', function () {
+            var config = confab([
+                confab.loadYaml()
+            ]);
+            expect(config).toEqual({});
+        });
+
+        it('has right author', function () {
+            var config = confab([
+                confab.loadYaml([
+                    yamlFixturePath('test')
+                ])
+            ]);
+            expect(config.author).toEqual('Lewis Carroll');
+        });
+
+        it('accepts a single file as a string', function () {
+            var config = confab([
+                confab.loadYaml(yamlFixturePath('test'))
+            ]);
+            expect(config.author).toEqual('Lewis Carroll');
+        });
+
+        it('skips missing files', function () {
+            var config = confab([
+                confab.loadYaml([
+                    yamlFixturePath('missing'),
+                    yamlFixturePath('test')
+                ])
+            ]);
+            expect(config.author).toEqual('Lewis Carroll');
+        });
+
+        it('throws when config is not parseable', function () {
+            var shouldThrow = function () {
+                confab([
+                    confab.loadYaml(yamlFixturePath('invalid'))
+                ]);
+            };
+
+            expect(shouldThrow).toThrow();
+        });
+
+        it('does not clobber existing config', function () {
+            var config = confab([
+                confab.assign({
+                    'extra': 'anything'
+                }),
+                confab.loadYaml(yamlFixturePath('test'))
+            ]);
+            expect(config.extra).toEqual('anything');
+        });
+
     });
-
-    it('has right author', function () {
-      var config = confab([
-        confab.loadYaml([
-          yamlFixturePath('test')
-        ])
-      ]);
-      expect(config.author).toEqual('Lewis Carroll');
-    });
-
-    it('accepts a single file as a string', function () {
-      var config = confab([
-        confab.loadYaml(yamlFixturePath('test'))
-      ]);
-      expect(config.author).toEqual('Lewis Carroll');
-    });
-
-    it('skips missing files', function () {
-      var config = confab([
-        confab.loadYaml([
-          yamlFixturePath('missing'),
-          yamlFixturePath('test')
-        ])
-      ]);
-      expect(config.author).toEqual('Lewis Carroll');
-    });
-
-    it('throws when config is not parseable', function () {
-      var shouldThrow = function () {
-        confab([
-          confab.loadYaml(yamlFixturePath('invalid'))
-        ]);
-      };
-
-      expect(shouldThrow).toThrow();
-    });
-
-    it('does not clobber existing config', function () {
-      var config = confab([
-        confab.assign({
-          'extra': 'anything'
-        }),
-        confab.loadYaml(yamlFixturePath('test'))
-      ]);
-      expect(config.extra).toEqual('anything');
-    });
-
-  });
 
 });
